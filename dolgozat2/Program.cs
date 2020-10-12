b11b9e6908901d7ca92fb78d17baf714ca0e7ba4
doboskornel <70878431+doboskornel@users.noreply.github.com>
2020-10-12 09:14:01 +0200

    Add files via upload

---
+++ dolgozat2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[100];
+            Random r = new Random();
+            int osszeg = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                r.Next();
+            }
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                osszeg += tomb.Length;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
